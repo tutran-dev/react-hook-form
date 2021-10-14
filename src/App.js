d67480a2591584f5a6d65ab112a4a0427990f319
iamtu <trandinhtu.dev@gmail.com>
2021-10-14 16:25:56 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import "./App.css";
 
 export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // console.log(errors)
+  const onSubmit = (data) => console.log(data);
   return (
-    <form>
-      <h1>React Hook Form</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Formik</h1>
       <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" placeholder="Your Name" />
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Your Name"
+        {...register("name", {
+          required: true,
+        })}
+      />
       <label htmlFor="name">Password</label>
       <input
         type="password"
         name="password"
         id="password"
         placeholder="Password"
+        {...register("password", {
+          required: true,
+          minLength: 6,
+        })}
+        // defaultValue="hello"
       />
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" placeholder="Your Email" />
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Your Email"
+        {...register("email", {
+          required: true,
+          pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        })}
+      />
 
-      <ul className="error-container">
-        <li>Name is required</li>
-      </ul>
+      {Object.keys(errors).length !== 0 && (
+        <ul className="error-container">
+          {errors.name?.type === "required" && <li>Name is required</li>}
+          {errors.password?.type === "required" && (
+            <li>Password is required</li>
+          )}
+          {errors.password?.type === "minLength" && (
+            <li>Password must be 6 characters long</li>
+          )}
+          {errors.email?.type === "required" && <li>Email is required</li>}
+          {errors.email?.type === "pattern" && <li>Invalid Email Address</li>}
+        </ul>
+      )}
 
       <button type="submit">Submit</button>
     </form>
